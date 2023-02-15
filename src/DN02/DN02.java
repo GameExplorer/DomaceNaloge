a075fbd9fc1d4f2a5a259593d421db47676806a5
GameExplorer <73129125+GameExplorer@users.noreply.github.com>
2023-02-15 14:00:00 -0600

    misc

--- src/DN02/DN02.java
+++ src/DN02/DN02.java
@@ -3,31 +3,32 @@ package DN02;
 public class DN02 {
     public static void main(String[] args) {
         //args = new String[] {"jesen zima pomlad poletje"};
-
+        String zvezda = "*";
         if(args.length == 0) System.out.println("Napaka pri uporabi programa!");
 
         else {
             //izpis obrobe v prvi vrstici
             for (int i = 0; i < args.length; i++) {
-                for(int j = 0; j < args[i].length()+2; j++) {
-                    System.out.print("*");
-                }
+
+                    System.out.print(zvezda.repeat(args[i].length()+ 2));
+
             }
 
             System.out.println();
 
+            System.out.print("* ");
             //* besede * izpis besedila
-            System.out.print("*");
             for(int i = 0; i < args.length; i++) {
-                System.out.print(" " + args[i]);
+                    System.out.print(args[i]+ " ");
             }
-            System.out.print(" *");
+            System.out.print("* ");
 
             System.out.println();
 
             //izpis obrobe v zadnje vrstice
             for (int i = 0; i < args.length; i++) {
-                for(int j = 0; j < args[i].length()+2; j++) System.out.print("*");
+                    System.out.print(zvezda.repeat(args[i].length()+ 2));
+
             }
         }
     }
