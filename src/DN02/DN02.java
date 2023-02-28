1bedabd97de4c211b0efd5f760c48b3add1490ac
GameExplorer <73129125+GameExplorer@users.noreply.github.com>
2023-02-28 20:59:45 +0100

    DN02
    - DN02

--- src/DN02/DN02.java
+++ src/DN02/DN02.java
@@ -2,34 +2,29 @@ package DN02;
 
 public class DN02 {
     public static void main(String[] args) {
-        //args = new String[] {"jesen zima pomlad poletje"};
-        String zvezda = "*";
-        if(args.length == 0) System.out.println("Napaka pri uporabi programa!");
+        String zvezdice = "*";
+        int dolzina = 0;
+        if (args.length == 0) System.out.println("Napaka pri uporabi programa!");
 
-        else {
-            //izpis obrobe v prvi vrstici
-            for (int i = 0; i < args.length; i++) {
+        else if (args.length == 1) { //za enostavčne argumente
+            String arg = args[0];
+            dolzina = arg.length() + 4; //+4 za 2 zvezdici na vsako stran
+            zvezdice = "*".repeat(dolzina);
+            System.out.printf(" %s%n * %s *%n %s", zvezdice, arg, zvezdice);
 
-                    System.out.print(zvezda.repeat(args[i].length()+ 2));
-
+        } else { //za večstavčne argumente
+            StringBuilder sb = new StringBuilder();
+            int dolzinaArgumenta = 1; //vključno s presledki
+            for (String arg : args) {
+                sb.append(arg).append(" ");
+                dolzinaArgumenta += arg.length() + 1;
             }
+            dolzina = dolzinaArgumenta + 2;
+            zvezdice = "*".repeat(dolzina);
+            System.out.printf(" %s%n * %s *%n %s", zvezdice, sb.toString().trim(), zvezdice);
 
-            System.out.println();
-
-            System.out.print("* ");
-            //* besede * izpis besedila
-            for(int i = 0; i < args.length; i++) {
-                    System.out.print(args[i]+ " ");
-            }
-            System.out.print("* ");
-
-            System.out.println();
-
-            //izpis obrobe v zadnje vrstice
-            for (int i = 0; i < args.length; i++) {
-                    System.out.print(zvezda.repeat(args[i].length()+ 2));
-
-            }
         }
+
     }
+
 }
