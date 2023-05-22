9efeef031469c835e0841da24f94888d5eefeb40
GameExplorer <73129125+GameExplorer@users.noreply.github.com>
2023-05-22 17:04:18 +0200

    Update DN11.java

--- src/DN11/DN11.java
+++ src/DN11/DN11.java
@@ -1,22 +1,20 @@
 package DN11;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
 
 /**
- * Kraj razred z imeno, kratico
- *
+ * Kraj razred
+ * Predstavlja eno mesto v železniški ponudbi. Vsebuje String ime kraja, String kratico (dvočrkovna kratica) ter seznam
+ * odhodov vlakov v sosednje kraje
  */
 class Kraj {
-    private String ime;
-    private String kratica;
-    private List<Vlak> odhodi;
+    private final String ime;
+    private final String kratica;
+    private final List<Vlak> odhodi;
 
     public Kraj(String ime, String kratica) {
         this.ime = ime;
@@ -45,11 +43,18 @@ class Kraj {
     }
 }
 
+/**
+ * Razred Vlak
+ * Predstavlja vlake iz poletne ponudbe, ki povezujejo dva kraja. Vsebuje oznako vlaka(idVlak), zacetniKraj, koncniKraj
+ * in trajanjeVoznje.
+ * Vsebuje abstraktni metodi String opis() in double cenaVoznje(). Opis vrne niz ali gre za regionalni ali za ekspresni
+ * vlak, cenaVoznje pa izračuna in vrne končno ceno vozovnice.
+ */
 abstract class Vlak {
-    private String idVlak;
-    private Kraj zacetniKraj;
-    private Kraj koncniKraj;
-    private int trajanjeVoznje;
+    private final String idVlak;
+    private final Kraj zacetniKraj;
+    private final Kraj koncniKraj;
+    private final int trajanjeVoznje;
 
     public Vlak(String idVlak, Kraj zacetniKraj, Kraj koncniKraj, int trajanjeVoznje) {
         this.idVlak = idVlak;
@@ -79,23 +84,28 @@ abstract class Vlak {
 
     public abstract double cenaVoznje();
 
+
     public String toString() {
         return String.format(
                 "Vlak %s (%s) %s -- %s (%s) (%d min, %.2f EUR)",
                 idVlak, opis(), zacetniKraj.getIme(), koncniKraj.getIme(), zacetniKraj.getKratica(),
-                getTrajanjeVoznje(), cenaVoznje()
+                    getTrajanjeVoznje(), cenaVoznje()
         );
     }
 }
 
+/**
+ * Razred RegionalniVlak
+ * Regionalni vlaki vozijo s povp hitrostjo 50km/h.
+ * Cena na km je 0.068
+ * metoda cenaVoznje() vrne izračunano ceno vozovnice
+ */
 class RegionalniVlak extends Vlak {
-    private int hitrost;
-    private double cenaNaKm;
+    private static final double HITROST = 50;
+    private static final double CenaNaKm = 0.068;
 
     public RegionalniVlak(String oznaka, Kraj zacetek, Kraj konec, int trajanje) {
         super(oznaka, zacetek, konec, trajanje);
-        this.hitrost = 50;
-        this.cenaNaKm = 0.068;
     }
 
     @Override
@@ -105,21 +115,25 @@ class RegionalniVlak extends Vlak {
 
     @Override
     public double cenaVoznje() {
-        double razdalja = (double) (hitrost * super.getTrajanjeVoznje()) / 60;
-        double cena = razdalja * this.cenaNaKm;
+        double razdalja = (HITROST * super.getTrajanjeVoznje()) / 60;
+        double cena = razdalja * CenaNaKm;
         return cena;
     }
 }
 
+/**
+ * Razred EkspresniVlak
+ * Regionalni vlaki vozijo s povp hitrostjo 50km/h.
+ * Cena na km je 0.068
+ * metoda cenaVoznje() vrne izračunano ceno vozovnice
+ */
 class EkspresniVlak extends Vlak {
-    private int hitrost;
-    private double cenaNaKm;
+    private static final int HITROST = 110;
+    private static final double CenaNaKm = 0.154;
     private double dopl;
 
     public EkspresniVlak(String oznaka, Kraj zacetek, Kraj konec, int trajanje, double dopl) {
         super(oznaka, zacetek, konec, trajanje);
-        this.hitrost = 110;
-        this.cenaNaKm = 0.154;
         this.dopl = dopl;
     }
 
@@ -130,12 +144,20 @@ class EkspresniVlak extends Vlak {
 
     @Override
     public double cenaVoznje() {
-        double razdalja = (double) (hitrost * super.getTrajanjeVoznje()) / 60;
-        double cena = razdalja * cenaNaKm + dopl;
+        double razdalja = (double) (HITROST * super.getTrajanjeVoznje()) / 60;
+        double cena = razdalja * CenaNaKm + dopl;
         return cena;
     }
 }
 
+/**
+ * Razred EuroRail
+ * Predstavlja celotno želežniško omrežje. Vsebuje zbirko vseh krajev in zbirko vseh vlakov.
+ * metoda preberiKraje(String ime datoteke) prrebere podatke o krajih ter jih zapiše v zbirko
+ * metoda preberiPovezave(String ime datoteke) prebere podaatke o vlakih, ter jih zapiše v zbirko vlakov
+ * metoda izpisiKraje() izpiše vse prebrane kraje
+ * metoda izpisiPovezave() izpiše vse prebrane vlake in podatke o trajanju in ceni vožnje
+ */
 class EuroRail {
     private List<Kraj> kraji;
     private List<Vlak> vlaki;
@@ -172,17 +194,17 @@ class EuroRail {
                     continue;
                 }
 
-                String imeKraja = podatki[0];
-                String kodaDrzave = podatki[1];
+                String imeKraja = podatki[0].trim();
+                String kodaDrzave = podatki[1].trim();
 
                 Kraj kraj = new Kraj(imeKraja, kodaDrzave);
                 kraji.add(kraj);
             }
 
-            return true;
+            return true; //Uspešno branje
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return false; //Napaka pri branju
         }
     }
 
@@ -194,39 +216,39 @@ class EuroRail {
     }
 
     public boolean preberiPovezave(String imeDatoteke) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(imeDatoteke));
+        try (BufferedReader br = new BufferedReader(new FileReader(imeDatoteke)))
+        {
             String vrstica;
             while ((vrstica = br.readLine()) != null) {
                 String[] podatki = vrstica.split(";");
-                if (podatki.length >= 4) {
-                    String oznakaVlaka = podatki[0].trim();
-                    String zacetniKraj = podatki[1].trim();
-                    String koncniKraj = podatki[2].trim();
-                    String trajanje = podatki[3].trim();
-                    if (krajObstaja(zacetniKraj) && krajObstaja(koncniKraj) && !zacetniKraj.equals(koncniKraj)) {
-                        int ure = 0;
-                        int minute = 0;
-                        if (trajanje.contains(".")) {
-                            String[] cas = trajanje.split("\\.");
-                            if (cas.length >= 2) {
-                                ure = Integer.parseInt(cas[0]);
-                                minute = Integer.parseInt(cas[1]);
-                            }
-                        } else {
-                            minute = Integer.parseInt(trajanje);
-                        }
+                if(podatki.length < 4) continue;
 
-                        if (podatki.length == 5) {
-                            double doplacak = Double.parseDouble(podatki[4].trim());
-                            EkspresniVlak vlak = new EkspresniVlak(oznakaVlaka, najdiKraj(zacetniKraj), najdiKraj(koncniKraj), ure, minute);
-                            dodajVlak(vlak);
-                        } else {
-                            RegionalniVlak vlak = new RegionalniVlak(oznakaVlaka, najdiKraj(zacetniKraj), najdiKraj(koncniKraj), Integer.parseInt(trajanje));
-                            dodajVlak(vlak);
-                        }
-                    }
+                String oznakaVlaka = podatki[0].trim();
+                String zacetniKraj = podatki[1].trim();
+                String koncniKraj = podatki[2].trim();
+                double trajanje = Double.parseDouble((podatki[3].trim()));
+
+                Vlak vlak;
+                Kraj zacetniKrajIme = null;
+                Kraj koncniKrajIme = null;
+
+                for(Kraj kraj : kraji) {
+                    if(kraj.getIme().equalsIgnoreCase(zacetniKraj)) zacetniKrajIme = kraj;
+                    if(kraj.getIme().equalsIgnoreCase(koncniKraj)) koncniKrajIme = kraj;
                 }
+
+                if(zacetniKrajIme == null || koncniKrajIme == null || zacetniKrajIme.equals(koncniKrajIme)) {
+                    continue;
+                }
+
+                if(podatki.length == 5) {
+                    double doplacanZnesek = Double.parseDouble(podatki[4].trim());
+                    vlak = new EkspresniVlak(oznakaVlaka, zacetniKrajIme, koncniKrajIme, (int) trajanje, doplacanZnesek);
+                }
+                else {
+                    vlak = new RegionalniVlak(oznakaVlaka, zacetniKrajIme, koncniKrajIme, (int) trajanje);
+                }
+                vlaki.add(vlak);
             }
             br.close();
             return true;
@@ -236,25 +258,8 @@ class EuroRail {
         }
     }
 
-    private boolean krajObstaja(String imeKraja) {
-        for (Kraj kraj : kraji) {
-            if (kraj.getIme().equals(imeKraja)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private Kraj najdiKraj(String imeKraja) {
-        for (Kraj kraj : kraji) {
-            if (kraj.getIme().equals(imeKraja)) {
-                return kraj;
-            }
-        }
-        return null;
-    }
-
     public void izpisiPovezave() {
+        System.out.println();
         System.out.println("Vlaki, ki povezujejo kraje:");
         for (Vlak vlak : vlaki) {
             System.out.println(vlak.toString());
@@ -262,6 +267,9 @@ class EuroRail {
     }
 }
 
+/**
+ * Metoda main izvede ustrezen del programa. V primeru da pokličemo nalogo, ki ne obstaja izpiše sporočilo o napaki
+ */
 public class DN11 {
     public static void main(String[] args) {
         //EuroRail euroRail;
@@ -309,7 +317,6 @@ public class DN11 {
         //        } else {
         //            System.out.println("Prišlo je do napake pri branju krajev in/ali povezav.");
         //        }
-
 
         int naloga = Integer.parseInt(args[0]);
         String krajiDatoteka = args[1];
